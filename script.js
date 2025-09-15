7d94a8825702440ecf8fc47f485c23fb90c80b92
anuragg1117 <158740633+anuragg1117@users.noreply.github.com>
2025-09-15 14:31:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
+const str = document.querySelectorAll(".st");
+
 function highlight() {
     //Write your code here
-
+	for(let i = 0;i<str.length;i++){
+		str[i].style.color = "rgb(0, 128, 0)";
+	}
 
 }
 
 
 function return_normal() {
     //Write your code here
-
+	for(let i = 0;i<str.length;i++){
+		str[i].style.color = "rgb(0, 0, 0)";
+	}
     
 }
